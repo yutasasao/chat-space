17863b4bba245e2acb6c5f7f6c8de9e986bd3bd3
yutasasao <ss.yuta@icloud.com>
2020-04-26 17:11:01 +0900

    first commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,45 @@
   $(function(){ 
     function buildHTML(message){
-     if ( message.image ) {
-       var html =
-        `<div class="main-chat__message-list__box">
-           <div class="main-chat__message-list__box__upper">
-             <div class="main-chat__message-list__box__upper__talker">
-               ${message.user_name}
-             </div>
-             <div class="main-chat__message-list__box__upper__date">
-               ${message.created_at}
-             </div>
-           </div>
-           <div class="main-chat__message-list__box__lower">
-             <p class="main-chat__message-list__box__lower__content">
-               ${message.content}
-             </p>
-           </div>
-           <img src=${message.image} >
-         </div>`
-       return html;
-     } else {
-      var html =
-      `<div class="main-chat__message-list__box">
-      <div class="main-chat__message-list__box__upper">
-        <div class="main-chat__message-list__box__upper__talker">
-          ${message.user_name}
-        </div>
-        <div class="main-chat__message-list__box__upper__date">
-          ${message.created_at}
-        </div>
-      </div>
-      <div class="main-chat__message-list__box__lower">
-        <p class="main-chat__message-list__box__lower__content">
-          ${message.content}
-        </p>
-      </div>
-    </div>`
-     return html;
-     };
-   }
-
+      if ( message.image ) {
+          var html =
+           `<div class="main-chat__message-list__box" data-message-id=${message.id}>
+              <div class="main-chat__message-list__box__upper">
+                <div class="main-chat__message-list__box__upper__talker">
+                  ${message.user_name}
+                </div>
+                <div class="main-chat__message-list__box__upper__date">
+                  ${message.created_at}
+                </div>
+              </div>
+              <div class="main-chat__message-list__box__lower">
+                <p class="main-chat__message-list__box__lower__content">
+                  ${message.content}
+                </p>
+              </div>
+              <img src=${message.image} >
+            </div>`
+          return html;
+        } else {
+          var html =
+           `<div class="main-chat__message-list__box" data-message-id=${message.id}>
+              <div class="main-chat__message-list__box__upper">
+                <div class="main-chat__message-list__box__upper__talker">
+                  ${message.user_name}
+                </div>
+                <div class="main-chat__message-list__box__upper__date">
+                  ${message.created_at}
+                </div>
+              </div>
+              <div class="main-chat__message-list__box__lower">
+                <p class="main-chat__message-list__box__lower__content">
+                  ${message.content}
+                </p>
+              </div>
+            </div>`
+          return html;
+        };
+      }
+   
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -52,6 +52,7 @@
       processData: false,
       contentType: false
     })
+
     .done(function(data){
       var html = buildHTML(data);
       $('.main-chat__message-list').append(html);
@@ -64,4 +65,32 @@
   });
   })
 
+  var reloadMessages = function() {
+    var last_message_id = $('.main-chat__message-list__box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+       var insertHTML = '';
+       $.each(messages, function(i, message) {
+         insertHTML += buildHTML(message)
+       });
+       $('.main-chat__message-list').append(insertHTML);
+       $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+      
+    })
+
+    .fail(function() {
+      alert('error');
+    });
+
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
